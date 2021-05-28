a5bf4259d8f8951904284a4954eaad6bf9a1d7d2
stevebroll <stevebroll@outlook.com>
2021-05-28 13:32:19 -0400

    add blb_mix function skeleton

---
+++ src/blb_mix.cpp
@@ -0,0 +1,36 @@
+#include <Rcpp.h>
+#include <stdlib.h>
+using namespace Rcpp;
+
+// [[Rcpp::export]]
+
+List blb_mix(NumericVector y, int b, int s, int r, int d){
+  NumericMatrix mu_lower(s,d);
+  NumericMatrix mu_upper(s,d);
+  NumericMatrix sd_lower(s,d);
+  NumericMatrix sd_upper(s,d);
+  NumericMatrix pi_lower(s,d);
+  NumericMatrix pi_upper(s,d);
+  //const int n = y.size();
+
+  for (int j=0; j < s; ++j) {
+
+    // Sample Here
+
+
+    NumericMatrix fit_mu(r,d);
+    NumericMatrix fit_sd(r,d);
+    NumericMatrix fit_pi(r,d);
+
+    for(int k=0; k < r; ++k) {
+      // Call gmm.cpp
+    }
+
+    // means and CIs here
+  }
+
+  // Return list here
+
+
+  return(0);
+}
