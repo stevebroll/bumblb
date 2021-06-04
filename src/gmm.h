be5efc063a7ccfcb4ef83faf030c810f31a83c20
stevebroll <stevebroll@outlook.com>
2021-06-04 13:55:50 -0500

    update gmm.h to match updated gmm.cpp

--- src/gmm.h
+++ src/gmm.h
@@ -2,9 +2,12 @@
 #define GMM_H
 #include <Rcpp.h>
 
-Rcpp::StringVector gmm(Rcpp::NumericVector X, int d,
-                                 Rcpp::Nullable<Rcpp::NumericVector> pi_ = R_NilValue,
-                                 Rcpp::Nullable<Rcpp::NumericVector> mu_ = R_NilValue,
-                                 Rcpp::Nullable<Rcpp::NumericVector> sd_ = R_NilValue,
-                                 int max_iter = 10000, double tol = 1e-5);
+Rcpp::List gmm(Rcpp::NumericVector X, int d,
+               Rcpp::Nullable<Rcpp::NumericVector> pi_ = R_NilValue,
+               Rcpp::Nullable<Rcpp::NumericVector> mu_ = R_NilValue,
+               Rcpp::Nullable<Rcpp::NumericVector> sd_ = R_NilValue,
+               int max_iter = 10000, double tol = 1e-5,
+               double beta = 1.0, double c = 1.1,
+               Rcpp::Nullable<Rcpp::NumericVector> schedule_ = R_NilValue);
+
 #endif
